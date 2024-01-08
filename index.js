452dd05053745c577bfaaa672a4c6f95554c5760
JooMin Choi <joominchoi23@gmail.com>
2024-01-08 11:03:00 +0000

    Create append and print functions

---
+++ index.js
@@ -0,0 +1,9 @@
+import LinkedList from "./linkedList.js";
+
+const linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.print();
--- linkedList.js
+++ linkedList.js
@@ -1,7 +1,29 @@
-import Node from "./node";
+import Node from "./node.js";
 
-class LinkedList {
+export default class LinkedList {
   constructor() {
     this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.nextNode) {
+        current = current.nextNode;
+      }
+      current.nextNode = newNode;
+    }
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.nextNode;
+    }
   }
 }
